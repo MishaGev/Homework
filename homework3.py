0b9a35528e2909fa3dcef19cd5ab4f19997e2569
MishaGev <misha96gevorgyan@gmail.com>
2024-05-21 20:28:41 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,7 @@
+name="Mikhail"
+print(name, type(name))
+age=27
+print(age, type(age))
+print(age + 1)
+name="is_student:"
+print(name, name ==name)
---
+++ homework4.py
@@ -0,0 +1,11 @@
+myString = input("Введите Ваше имя, добрый человек! ")
+print("Ой, какие люди! Добро пожаловать,", myString)
+currenYear = 2024
+dateOfBerth = int(input("В каком году появился ты на свет, дитя? "))
+age = currenYear - dateOfBerth
+print("Ого, да Вам в этом году ", age, "лет")
+print(myString.upper())
+print(myString.lower())
+print(myString.replace(" ", ""))
+print(myString[0])
+print(myString[-1])
