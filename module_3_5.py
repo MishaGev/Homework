e12374d5126c8cf314b7c5f37230ae23f43e409e
MishaGev <misha96gevorgyan@gmail.com>
2024-07-24 22:01:09 +0300

    Add files via upload

---
+++ module_3_5.py
@@ -0,0 +1,11 @@
+def get_multiplied_digits(number):
+    str_number = str(number)
+    first = int(str_number[0])
+    if len(str_number) > 1:
+        return  first * get_multiplied_digits(int(str_number[1:]))
+    else:
+        return first
+result1 = get_multiplied_digits(40203)
+result2 = get_multiplied_digits(500362087015)
+print(result1)
+print(result2)
