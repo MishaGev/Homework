b608226b7fd44971c0fe172e03cc02ee35aae54c
MishaGev <misha96gevorgyan@gmail.com>
2024-07-26 11:43:58 +0300

    Update module_3_4.py

--- module_3_4.py
+++ module_3_4.py
@@ -1,11 +1,11 @@
 def single_root_words(root_word, *other_words):
     same_words = []
     for word in other_words:
-        if root_word in word or word in root_word:
+        if root_word.lower() in word.lower() or word.lower() in root_word.lower():
             same_words.append(word)
     return same_words
 
-result1 = single_root_words("банан", "мандарин", "клубникабанан", "яблбананоко")
+result1 = single_root_words("Банан", "мандарин", "клуБникаБанан", "яблбананоко")
 result2 = single_root_words("car", "avtocar", "Михаил", "carгонщик")
 print(result1)
 print(result2)
